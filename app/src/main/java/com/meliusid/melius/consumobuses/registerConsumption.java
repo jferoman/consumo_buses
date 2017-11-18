67ae0bb10b97b2bff255c00bf671d15abafc501e
Duvan Castelblanco <duvancastelblanco@hotmail.com>
2017-11-18 13:25:18 -0500

    first commit

---
+++ app/src/main/java/com/meliusid/melius/consumobuses/registerConsumption.java
@@ -0,0 +1,16 @@
+package com.meliusid.melius.consumobuses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// REST API
+
+
+public class registerConsumption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_consumption);
+    }
+}
---
+++ app/src/main/java/logic/Bus.java
@@ -0,0 +1,24 @@
+package logic;
+
+/**
+ * Created by joseroman on 18/11/17.
+ */
+
+public class Bus {
+    private String code;
+
+    public Bus(){
+
+    }
+
+    public Bus (String code){
+        this.code = code;
+    }
+
+    public String getCode(){
+        return this.code;
+    }
+
+    public void setCode(String code){
+    }
+}
